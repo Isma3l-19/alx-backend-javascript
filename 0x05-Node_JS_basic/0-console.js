a3f75c44060934adeb978e1c62f1e209e15e6bfb
Ismael Nyambu <133232846+Isma3l-19@users.noreply.github.com>
2024-08-26 23:46:29 +0000

    Done with task 8

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,10 @@
-function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ismael Nyambu <https://github.com/Isma3l-19>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
